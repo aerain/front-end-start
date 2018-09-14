f2d9adeb778de65aa90e1a5cd182724fb1db0a5f
aerain <j40f893@gmail.com>
2018-09-14 22:38:58 +0900

    최종

--- 03/quiz/quiz.js
+++ 03/quiz/quiz.js
@@ -1,5 +1,6 @@
 class Quiz {
     constructor() {
+        this.COLUMN = 3;
         this.index = 0;
         this.container = document.getElementById('container');
         this.rownum = document.getElementById('rownum');
@@ -28,10 +29,27 @@ class Quiz {
                 "img":"http://icon.daumcdn.net/w/c/12/05/81730673405131839.jpeg",
                 "title":"&#39;내가 더 잘해&#39; 후보GK 경기 난입해 선방",
                 "id":"20120516100608409"
-            },{"url":"http://sports.media.daum.net/general/gallery/0516ufc/index.html","img":"http://icon.daumcdn.net/w/c/12/05/81728759258718839.jpeg","title":"양동이의 하이킥에 타바레스 &#39;주춤&#39;","id":"20120516093313331"}
-            ,{"url":"http://media.daum.net/entertain/photo/gallery/?gid=100539","img":"http://icon.daumcdn.net/w/c/12/05/81728404408992839.jpeg","title":"이승철 아내와 딸 사진 공개 &quot;사주에. .&quot;","id":"20120516093918544"}
-            ,{"url":"http://media.daum.net/photo/3899","img":"http://icon.daumcdn.net/w/c/12/05/81728227037306839.jpeg","title":"생후 6개월에 프랑스로 입양됐던 아이가..","id":"20120516030614331"}
-            ,{"url":"http://sports.media.daum.net/general/gallery/STARKIMYUNA/index.html","img":"http://icon.daumcdn.net/w/c/12/05/81727815537682839.jpeg","title":"&#39;교생&#39; 김연아, 스승의날에도 인기폭발","id":"20120516092003892"}
+            }, {
+                "url":"http://sports.media.daum.net/general/gallery/0516ufc/index.html",
+                "img":"http://icon.daumcdn.net/w/c/12/05/81728759258718839.jpeg",
+                "title":"양동이의 하이킥에 타바레스 &#39;주춤&#39;",
+                "id":"20120516093313331"
+            }, {
+                "url":"http://media.daum.net/entertain/photo/gallery/?gid=100539",
+                "img":"http://icon.daumcdn.net/w/c/12/05/81728404408992839.jpeg",
+                "title":"이승철 아내와 딸 사진 공개 &quot;사주에. .&quot;",
+                "id":"20120516093918544"
+            }, {
+                "url":"http://media.daum.net/photo/3899",
+                "img":"http://icon.daumcdn.net/w/c/12/05/81728227037306839.jpeg",
+                "title":"생후 6개월에 프랑스로 입양됐던 아이가..",
+                "id":"20120516030614331"
+            }, {
+                "url":"http://sports.media.daum.net/general/gallery/STARKIMYUNA/index.html",
+                "img":"http://icon.daumcdn.net/w/c/12/05/81727815537682839.jpeg",
+                "title":"&#39;교생&#39; 김연아, 스승의날에도 인기폭발",
+                "id":"20120516092003892"
+            }
         ];
 
         this.initialization();
@@ -71,7 +89,7 @@ class Quiz {
         this.str.push('');
         this.todayPhoto.map((item, index) => {
             this.str[this.str.length - 1] += this.imageContent(item.img, item.title);
-            if(index % 3 == 2)
+            if(index % this.COLUMN === this.COLUMN - 1)
                 this.str.push('');
         });
         this.showPage();
