fd726c20a6cc1d31ce3e59ead031602494f9e90d
aerain <j40f893@gmail.com>
2018-09-21 16:49:19 +0900

    한줄 코딩

--- 04/app.js
+++ 04/app.js
@@ -1,10 +1,16 @@
 var wrap = document.getElementsByClassName('wrap')[0];
 
+var str = '';
+
 for(let i = 1; i <= 4; i++) {
     for(let j = 1; j <= 4; j++) {
-        let block = document.createElement('div');
-        block.className = (i + j) % 2 == 1 ? 'black' : 'white';
-        wrap.appendChild(block);
+        // let block = document.createElement('div');
+        // block.className = (i + j) % 2 == 1 ? 'black' : 'white';
+        // wrap.appendChild(block);
+        str += `<div class='${(i + j) % 2 == 1 ? 'black' : 'white'}'></div>`
+        
     }
 }
+
+wrap.innerHTML = str;
 
