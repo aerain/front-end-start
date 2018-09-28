b4b73af1ff5c40decfd14aed8af0b38a878b858f
Aerain <aerain@Aerainui-MacBook-Pro.local>
2018-09-28 16:22:20 +0900

    변경

--- 04/app.js
+++ 04/app.js
@@ -10,6 +10,24 @@ for(let i = 1; i <= 4; i++) {
         str += `<div class='${(i + j) % 2 == 1 ? 'black' : 'white'}'></div>`  
     }
 }
-
 wrap.innerHTML = str;
+
+var boxes = document.querySelectorAll('.wrap > div');
+
+function select(event) {
+    var selected = document.querySelector('.yellow');
+    if(selected != null) {
+        selected.classList.remove('yellow');
+    }
+    let block = event.currentTarget;
+    block.classList.add('yellow');
+    console.log('됨');
+}
+
+for(let i = 0; i < boxes.length; i++) {
+    boxes[i].addEventListener('click', select);
+}
+
+
+
 
