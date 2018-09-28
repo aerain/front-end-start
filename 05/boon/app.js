843860ccf522e89510be8750d79a745033008b80
Aerain <aerain@Aerainui-MacBook-Pro.local>
2018-09-28 19:13:11 +0900

    클래스

--- 05/boon/app.js
+++ 05/boon/app.js
@@ -1,23 +1,41 @@
-var url = 'https://1boon.kakao.com/ch/enter.json?page=1&pagesize=10';
+var url = 'https://1boon.kakao.com/ch/enter.json';
 
-getUrlData(url, json => {
-    // console.log(json);
-    var str = '';
-    for(let i = 0; i < json.data.length; i++) {
-        let block = json.data[i];
-        str += `<a href="https://1boon.kakao.com/${block.path}">${block.title}</a><br>`
+class App {
+    constructor(url) {
+        this.url = url;
+        this.count = 1;
+        this.moreButton = document.querySelector('button');
+        
+        this.initialization();
     }
 
-    document.getElementById('wrap').innerHTML = str;
-});
+    initialization() {
+        this.moreButton.addEventListener('click', this.getData.bind(this));
+    }
 
-async function getUrlData(url, callback) {
-    try {
-        let data = await fetch(url);
-        let json = await data.json();
-        callback(json);
-        console.log('Fetch Success');
-    } catch (err) {
-        console.log('Fetch Error : -S', err);
+    getData() {
+        this.getUrlData(url, json => {
+            // console.log(json);
+            var str = '';
+            for(let i = 0; i < json.data.length; i++) {
+                let block = json.data[i];
+                str += `<a href="https://1boon.kakao.com/${block.path}">${block.title}</a><br>`
+            }
+        
+            document.getElementById('wrap').innerHTML += str;
+        });
+    }
+
+    async getUrlData(url, callback) {
+        try {
+            let data = await fetch(url + `?page=${this.count++}&pagesize=10`);
+            let json = await data.json();
+            callback(json);
+            console.log('Fetch Success');
+        } catch (err) {
+            console.log('Fetch Error : -S', err);
+        }
     }
 }
+
+new App(url).getData();
