9de3ac2f7cfcd012ff585205a333f516a5ae42de
aerain <j40f893@gmail.com>
2018-10-12 16:46:52 +0900

    검색어

--- 07/jquery.js
+++ 07/jquery.js
@@ -1,2 +1,23 @@
 // document.querySelector('h1').innerHTML = 'hi';
-$('h1').html('hi!');
+// $('h1').fadeOut().delay(1000).fadeIn();
+
+// var $div = $('div');
+
+// $('div')
+//     // .css('background-color', '#339955')
+//     .css('border', '1px solid red');
+
+// $('#box').addClass('red');
+// $('#box').toggleClass('red');
+
+var $btn = $('#btn');
+var $txt = $('#txt');
+var $debug = $('#box');
+$btn.on('click', event => {
+    console.log('click');
+    if($txt.val() === '') {
+        alert('검색어를 입력하세요');
+        return;
+    } 
+    $debug.html($txt.val());
+});
