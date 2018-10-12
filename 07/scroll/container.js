ffed0e99eda97d79cacfb72ea7da24e215aa6098
aerain <j40f893@gmail.com>
2018-10-12 18:04:53 +0900

    더보기

--- 07/scroll/container.js
+++ 07/scroll/container.js
@@ -1,19 +1,26 @@
 var $container = $('#container')
 
-var $top = $('<div>')
-.addClass('top')
-.html('위로가세요')
-.appendTo($container);
+var checkScroll = () => {
+    let scrollTop = $window.scrollTop();
+    let 업버튼이보여질위치 = 200;
 
-$(window).scroll(() => {
-    let y = window.pageYOffset;
-    if(y >= 200) {
+    if(scrollTop >= 업버튼이보여질위치) {
         $top.show('slow');
     } else {
         $top.hide('slow');
     }
-})
+}
+var goTop = event => {
+    // $window.scrollTop(0);
+    $('html, body').animate({scrollTop: 0}, 400);
+}
 
-$top.on('click', event => {
-    window.scrollTo(0, 0);
-})
+var $top = $('<div>')
+.addClass('top')
+.html('위로가세요')
+.appendTo($container);
+var $window = $(window);
+
+$window.scroll(checkScroll);
+
+$top.on('click', goTop);
