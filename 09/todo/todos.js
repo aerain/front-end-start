8dcc632a51fcdafb3d053b3cd887063efcaefabc
aerain <j40f893@gmail.com>
2018-10-26 17:26:54 +0900

    커밋

--- 09/todo/todos.js
+++ 09/todo/todos.js
@@ -2,15 +2,57 @@
 
 var input = document.querySelector('#memo');
 var form = document.querySelector('.new-task');
+var todoList = document.querySelector('#todo-list')
 
 function addTodo(event) {   
-    // if(event.keyCode === 13) {
-        let todo = input.value;
+    let todo = input.value;
+    if(todo.trim() !== '') {
         console.log(todo);
-    // }
+        insertTodo(todo);
+        input.value = "";
+    }
+    
 
     // 해당 url로 넘어가지 않게 동작하기
-    return false;
+    // return false;
+    // 혹은 동작하지 않게 하기
+    event.preventDefault();
+}
+function doneTodo(event) {
+    let parent = event.target.parentNode;
+    let todoText = parent.querySelector('span.text');
+    console.log(event.target);
+    if(event.target.value === "on") {
+        todoText.classList.add('done');
+    }
+    else {
+        todoText.classList.remove('done');
+    }
+        
+}
+
+function deleteTodo(event) {
+    todoList.removeChild(event.target.parentNode);
+}
+
+function insertTodo(todo) {
+    let todoLi = document.createElement('li');
+    todoLi.innerHTML = `
+        <button class="delete">x</button>
+        <input type="checkbox" class="toggle-checked">
+        <span class="text">${todo}</span>
+    `
+    todoLi.querySelector('.delete').addEventListener('click', deleteTodo);
+    console.log(todoLi.querySelector('.toggle-checked'));
+    todoLi.querySelector('.toggle-checked').addEventListener('click', doneTodo);
+    todoList.appendChild(todoLi);
+    // var todoBlock = `
+    //     <li>
+    //         <button class="delete">x</button>
+    //         <input type="checkbox" class="toggle-checked">
+    //         <span class="text">${todo}</span>
+    //     </li>
+    // `
 }
 
 // input.addEventListener('keydown', addTodo);
