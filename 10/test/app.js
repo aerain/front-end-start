1ee9ce8168240e7464ba102a505d383f95431efb
aerain <j40f893@gmail.com>
2018-11-02 17:22:17 +0900

    생명

--- 10/test/app.js
+++ 10/test/app.js
@@ -1,12 +1,15 @@
 var board = document.querySelector('#board'),
     levelSelector = document.querySelector('.level'),
-    scoreSelector = document.querySelector('.score');
+    scoreSelector = document.querySelector('.score'),
+    lifeSelector = document.querySelector('.life');
 
-// var levelTimer = 20000;
-var blockAppearTimer = 2000;
+var levelTimer = 5000,
+    blockAppearTimer = 2000,
+    levelUpReduceAppearTimer = 200;
 
 var score = 0,
-    level = 0;
+    level = 1,
+    life = 10;
 
 var boardWidth = 550,
     boardHeight = 550;
@@ -16,7 +19,24 @@ function addScore() {
     setScore();
 }
 function setScore() {
-    scoreSelector.innerText = `Score : ${score}`
+    scoreSelector.innerText = `Score : ${score}`;
+}
+
+function addLevel() {
+    level++;
+    setLevel();
+}
+
+function setLevel() {
+    levelSelector.innerText = `Level : ${level}`;
+}
+
+function reduceLife() {
+    life--;
+    setLife();
+}
+function setLife() {
+    lifeSelector.innerText = `Life : ${life}`;
 }
 
 function randomCoordinate() {
@@ -28,11 +48,17 @@ function randomCoordinate() {
     block.style.left = left + 'px';
 }
 
+function onTime() {
+    reduceLife();
+    randomCoordinate();
+}
 function placeBlock(event) {
     clearInterval(blockInterval);
     addScore();
     randomCoordinate();
+    blockInterval = setInterval(onTime, blockAppearTimer);
 }
+
 function createBlock() {
     let block = document.createElement('div');
     block.className = 'block';
@@ -41,13 +67,18 @@ function createBlock() {
     board.appendChild(block);
 }
 
-createBlock();
-
-var blockInterval = setInterval(randomCoordinate, blockAppearTimer);
-
 // function levelUp() {
+//     setLevel();
+//     blockAppearTimer -= levelUpReduceAppearTimer; // 레벨업 될때마다 ReduceTimer 만큼 깝니다. 
 
-//     blockAppearTimer -= 2000; // 레벨업 될때마다 2초씩 깝니다. 
+//     if(blockAppearTimer <= 0) {
+//         clearInterval(levelUp);
+//         alert('네!');
+//     }
 // }
 
-// setInterval(levelUp, levelTimer);
+
+createBlock();
+
+var blockInterval = setInterval(onTime, blockAppearTimer);
+var levelUp = setInterval(levelUp, levelTimer);
